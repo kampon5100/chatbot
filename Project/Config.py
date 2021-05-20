71b12cfe25d7a6405f35019bb068ebe5251baf0a
kampon5100 <kampon0jam@gmail.com>
2021-05-20 10:24:58 +0700

    Revert "change Config"
    This reverts commit 0aabc3061689774ee6e2074a71bfacee1499e2c6.

--- Project/Config.py
+++ Project/Config.py
@@ -2,10 +2,10 @@
 
 
 
-Channel_secret = "5698dc03982cf9203b5a273f5acf2af4"
+Channel_secret = "da180eac5e665afbfc0a40232d524049"
 
 
-Channel_access_token = "jIWZlHsWujqiU4WaJtCgnf18iQY7coZ4MlPmLwvorhg88nDWpczG3wSBjFfUNPXvCu8tZY9KnbZ+6Zgbr9wjQOnA3P9/XB5NlgJ/ZT4CeBJBClVpeTFatG86q9UyxF3a36tUPvMqNEiJCLJd0+YMVAdB04t89/1O/w1cDnyilFU="
+Channel_access_token = "tdW2cT7Rxu4MWnV3Z9nEy9EWIbsy3fFX8De+lX6xIiL5OU0YYLr5BugLvT3qZVNEtK9itMtnFcxkh0MFbisLCtmlHXWJHG43919vEHti6RnWPKJ6Cs3WfouwVwGntNL/zXhRSv3OTiefzpzXjUwsKQdB04t89/1O/w1cDnyilFU="
 
 
-Basic_ID = "@160sndql"
+Basic_ID = "@108lsrxj"
